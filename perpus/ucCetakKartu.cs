854f900491bd7d8168a1669d7dd3054c81cb5657
priyokan <priyokan316@gmail.com>
2019-08-22 10:46:09 +0700

    fixing image paramerter on print member card

--- perpus/ucCetakKartu.cs
+++ perpus/ucCetakKartu.cs
@@ -31,17 +31,18 @@ namespace perpus
             member memm = new member();
             memm = db.members.Single(x => x.nis == 3);
 
-            ReportParameter imgPar = new ReportParameter();
-            imgPar = new ReportParameter("report", "haha");
+            ReportParameter[] imgPar = new ReportParameter[1];
+            imgPar[0] = new ReportParameter("test", new Uri(memm.pohoto.ToString()).AbsoluteUri);
             reportViewer1.Reset();
             reportViewer1.ProcessingMode = ProcessingMode.Local;
             reportViewer1.LocalReport.DataSources.Clear();
-            reportViewer1.ZoomMode = ZoomMode.PageWidth;
             reportViewer1.LocalReport.EnableHyperlinks = true;
             reportViewer1.LocalReport.EnableExternalImages = true;
             reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", report));
             reportViewer1.LocalReport.ReportEmbeddedResource = "perpus.Report2.rdlc";
+            reportViewer1.LocalReport.SetParameters(imgPar);
             reportViewer1.Size = new Size(975, 419);
+            reportViewer1.ZoomMode = ZoomMode.PageWidth;
             reportViewer1.RefreshReport();
         }
     }
